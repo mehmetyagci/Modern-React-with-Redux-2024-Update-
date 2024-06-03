1bea020ff86034b849d716a6f9d9df793703b137
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-06-03 10:26:18 +0300

    section 43 has been completed.

---
+++ 03animals/src/AnimalShow.js
@@ -0,0 +1,5 @@
+function AnimalShow() {
+    return <div>COW!</div>
+}
+
+export default AnimalShow;
--- 03animals/src/App.js
+++ 03animals/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const handleClick = () => {
+        console.log('Button was clicked')
+    };
+
+    const handleMouseMove = () => {
+        console.log('Mouse moved inside of the button')
+    };
+
+    return (
+    <div>
+        <button onClick={handleClick} onMouseMove={handleMouseMove}>Add Animal</button>
+        </div>
+    )
 }
 
 export default App;
