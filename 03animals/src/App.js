e2a039736681eb5275ba4b65365f1988985e92fd
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-06-04 22:54:22 +0300

    section 4 has been completed.

--- 03animals/src/App.js
+++ 03animals/src/App.js
@@ -1,16 +1,28 @@
+import "./App.css";
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+  
+  return animals[Math.floor(Math.random() * animals.length)];
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [animals, setAnimals] = useState([]);
+  
   const handleClick = () => {
-    setCount(count + 1);
+     setAnimals([...animals, getRandomAnimal()]);
   };
 
+  const renderedAnimals = animals.map((animal, index) => {
+    return <AnimalShow key={index} type={animal} />
+  })
+
   return (
-    <div>
+    <div className="app">
       <button onClick={handleClick}>Add Animal</button>
-      <div>Number of animals: {count}</div>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
   );
 }
