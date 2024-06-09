ec81d406cd644d82708c9179d244764d8adca95f
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-06-09 22:11:49 +0300

    70. Handling Form Submission has been completed.

--- 04pics/src/App.js
+++ 04pics/src/App.js
@@ -1,5 +1,16 @@
-function App(){
-    return <div>App</div>
+import SearchBar from "./components/SearchBar";
+
+function App() {
+
+  const handleSubmit = (term) => {
+    console.log('Do a search with', term);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
--- 04pics/src/components/SearchBar.js
+++ 04pics/src/components/SearchBar.js
@@ -1,5 +1,17 @@
-function SearchBar(){
-    return <div>SearchBar</div>
+function SearchBar({ onSubmit }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    onSubmit('cars');
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
