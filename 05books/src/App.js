2d0e2194bf604b2c7cc7c60573c939ae76d1b26d
CORP-AD\w191003 <mehmet.yagci@wordline.com>
2024-07-09 23:52:44 +0300

    131. Deleting a Record has been completed.

--- 05books/src/App.js
+++ 05books/src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import axios from "axios";
 
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
@@ -6,11 +8,26 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
-  const editBookById = (id, newTitle) => {
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+
+    setBooks(response.data);
+  };
+
+  const editBookById = async (id, newTitle) => {
     console.log("Need to edit book with id:", id);
+
+    const response = await axios.put(`http://localhost:3001/books/${id}`, { title: newTitle });
+
+    console.log(response);
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
 
       return book;
@@ -19,8 +36,11 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const deleteBookById = (id) => {
-    console.log("Need to delete book with id:");
+  const deleteBookById = async (id) => {
+    console.log("Need to delete book with id:", id);
+
+    await axios.delete(`http://localhost:3001/books/${id}`);
+    
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -28,26 +48,19 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
+  const createBook = async (title) => {
     console.log("Need to add book with:", title);
-    const updatedBooks = [
-      ...books,
-      {
-        id: Math.round(Math.random() * 9999),
-        title,
-      },
-    ];
+
+    const response = await axios.post("http://localhost:3001/books", { title });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList
-        books={books}
-        onEdit={editBookById}
-        onDelete={deleteBookById}
-      />
+      <BookList books={books} onEdit={editBookById} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
